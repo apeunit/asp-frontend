37e0b9a6be8bb122ce9a1a89a9b44028d5e20909
Bernd Große Lordemann <bernd@newnow.de>
2024-02-21 14:29:20 +0100

    feat: radix basic install

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import AuthCard from '@/app/(auth)/AuthCard'
-import ApplicationLogo from '@/components/ApplicationLogo'
 
 export const metadata = {
     title: 'Laravel',
@@ -13,7 +12,7 @@ const Layout = ({ children }) => {
                 <AuthCard
                     logo={
                         <Link href="/">
-                            <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
+                            LOGO
                         </Link>
                     }>
                     {children}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,19 @@
+import '@radix-ui/themes/styles.css'
 import '@/app/global.css'
-
+import { Theme, Flex, Text, Button } from '@radix-ui/themes'
 export const metadata = {
     title: 'Laravel',
 }
 const RootLayout = ({ children }) => {
     return (
         <html lang="en">
-            <body className="antialiased">{children}</body>
+            <body className="antialiased light">
+                <Theme>{children}</Theme>
+                <Flex direction="column" gap="2">
+                    <Text>Hello from Radix Themes :)</Text>
+                    <Button>Let's go</Button>
+                </Flex>
+            </body>
         </html>
     )
 }
