e334a799e8c7dadd29ec53e4530ef4ea7fa8b4a9
Bernd Große Lordemann <bernd@newnow.de>
2024-02-22 15:57:58 +0100

    feat: small fe refinements

--- src/app/(app)/Navigation.js
+++ src/app/(app)/Navigation.js
@@ -21,12 +21,6 @@ const Navigation = ({ user }) => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
                     <div className="flex">
-                        {/* Logo */}
-                        <div className="flex-shrink-0 flex items-center">
-                            <Link href="/dashboard">
-                                <ApplicationLogo className="block h-10 w-auto fill-current text-gray-600" />
-                            </Link>
-                        </div>
 
                         {/* Navigation Links */}
                         <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -9,12 +9,7 @@ const Layout = ({ children }) => {
     return (
         <div>
             <div className="font-sans text-gray-900 antialiased">
-                <AuthCard
-                    logo={
-                        <Link href="/">
-                            LOGO
-                        </Link>
-                    }>
+                <AuthCard>
                     {children}
                 </AuthCard>
             </div>
