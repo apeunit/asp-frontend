5e613ced041807d22ff0bcc65e4addf14f5d93c1
Bernd Große Lordemann <bernd@newnow.de>
2024-03-01 14:32:49 +0100

    test: email-verification

--- app/(auth)/verify-email/page.tsx
+++ app/(auth)/verify-email/page.tsx
@@ -1,8 +1,19 @@
 "use client";
 
-import Button from "@/components/shared/Button";
 import { useAuth } from "../../../hooks/auth";
 import { useState } from "react";
+import styles from "./Verify.module.css";
+import {
+  Box,
+  Button,
+  Card,
+  Checkbox,
+  Flex,
+  Heading,
+  Link,
+  Text,
+  TextField,
+} from "@radix-ui/themes";
 
 const Page = () => {
   const { logout, resendEmailVerification } = useAuth({
@@ -14,32 +25,33 @@ const Page = () => {
 
   return (
     <>
-      <div className="mb-4 text-sm text-gray-600">
-        Thanks for signing up! Before getting started, could you verify verify
-        your email address by clicking on the link we just emailed to you? If
-        you didn't receive the email, we will gladly gladly send you another.
-      </div>
+      <Card className={styles.card} variant="surface">
+        <Flex direction="column" gap="4" align="center">
+          <img src="/favicon.png" width={72} alt="ASP App Icon" />
+          <Heading size="6" weight="medium">
+            Email Verification!
+          </Heading>
+          <div className="mb-4 text-sm text-gray-600">
+            Thanks for signing up! Before getting started, could you verify
+            verify your email address by clicking on the link we just emailed to
+            you? If you didn't receive the email, we will gladly send you
+            another.
+          </div>
 
-      {status === "verification-link-sent" && (
-        <div className="mb-4 font-medium text-sm text-green-600">
-          A new verification link has been sent to the email address address you
-          provided during registration.
-        </div>
-      )}
+          {status === "verification-link-sent" && (
+            <div className="mb-4 font-medium text-sm text-green-600">
+              A new verification link has been sent to the email address address
+              you provided during registration.
+            </div>
+          )}
 
-      <div className="mt-4 flex items-center justify-between">
-        <Button onClick={() => resendEmailVerification({ setStatus })}>
-          Resend Verification Email
-        </Button>
-
-        <button
-          type="button"
-          className="underline text-sm text-gray-600 hover:text-gray-900"
-          onClick={logout}
-        >
-          Logout
-        </button>
-      </div>
+          <div className="mt-4 flex items-center justify-between">
+            <Button onClick={() => resendEmailVerification({ setStatus })}>
+              Resend Verification Email
+            </Button>
+          </div>
+        </Flex>
+      </Card>
     </>
   );
 };
