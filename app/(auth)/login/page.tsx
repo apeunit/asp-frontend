d97292f60edac2f22577c0ce6d359f301021025a
Marcel Schulz <mail@schulzmarcel.de>
2024-03-06 13:11:39 +0000

    Create custom InputField component & deprecate radix-input

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import InputError from "@/components/shared/InputError";
-import Label from "@/components/shared/Label";
 import { useAuth } from "../../../hooks/auth";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
@@ -9,8 +7,9 @@ import AuthSessionStatus from "../AuthSessionStatus";
 
 import styles from "./Login.module.css";
 
-import { Button, Link, Text, TextField } from "@radix-ui/themes";
+import { Button, Link } from "@radix-ui/themes";
 import AuthCard from "../AuthCard/AuthCard";
+import InputField from "@/components/shared/InputField/InputField";
 
 const Login = () => {
   const router = useRouter();
@@ -51,44 +50,31 @@ const Login = () => {
     <AuthCard className={styles.card} title={"Willkommen!"}>
       <form onSubmit={submitForm}>
         {/* Email Address */}
-        <div className={styles.field}>
-          <Label htmlFor="email">
-            <Text weight="medium">Email</Text>
-          </Label>
+        <InputField
+          label="E-Mail"
+          id="email"
+          type="email"
+          placeholder={"name@work-email.com"}
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          errorMessages={errors.email}
+          className={styles.field}
+          required
+          autoFocus
+        />
 
-          <TextField.Input
-            id="email"
-            type="email"
-            size={"3"}
-            placeholder={"name@work-email.com"}
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            required
-            autoFocus
-          />
-
-          <InputError messages={errors.email} className="mt-2" />
-        </div>
-
-        {/* Password */}
-        <div className={styles.field}>
-          <Label htmlFor="password">
-            <Text weight="medium">Password</Text>
-          </Label>
-
-          <TextField.Input
-            id="password"
-            type="password"
-            size={"3"}
-            placeholder={"Enter Password"}
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            required
-            autoComplete="current-password"
-          />
-
-          <InputError messages={errors.password} className="mt-2" />
-        </div>
+        <InputField
+          label="Password"
+          id="password"
+          type="password"
+          size={"3"}
+          placeholder={"Enter Password"}
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          required
+          errorMessages={errors.password}
+          autoComplete="current-password"
+        />
 
         <Link href="/forgot-password" className={styles.forgotPassword}>
           Forgot password?
--- app/(auth)/password-reset/[token]/page.tsx
+++ app/(auth)/password-reset/[token]/page.tsx
@@ -1,15 +1,14 @@
 'use client'
 
-import InputError from "@/components/shared/InputError";
-import Label from "@/components/shared/Label";
 import { useAuth } from "../../../../hooks/auth";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
-import { Button, Text, TextField } from "@radix-ui/themes";
+import { Button } from "@radix-ui/themes";
 
 import styles from "./PasswordReset.module.css";
 import AuthCard from "../../AuthCard/AuthCard";
+import InputField from "@/components/shared/InputField/InputField";
 
 const PasswordReset = () => {
   const searchParams = useSearchParams();
@@ -41,65 +40,41 @@ const PasswordReset = () => {
   return (
     <AuthCard className={styles.card} title={"Reset Password"}>
       <form onSubmit={submitForm}>
-        {/* Email Address */}
-        <div className={styles.field}>
-          <Label htmlFor="email">
-            <Text weight="medium">Email</Text>
-          </Label>
+        <InputField
+          label="E-Mail"
+          id="email"
+          type="email"
+          size={"3"}
+          placeholder={"name@work-email.com"}
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          required
+          autoFocus
+        />
 
-          <TextField.Input
-            id="email"
-            type="email"
-            size={"3"}
-            placeholder={"name@work-email.com"}
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            required
-            autoFocus
-          />
+        <InputField
+          label="Password"
+          id="password"
+          type="password"
+          placeholder={"Enter Password"}
+          value={password}
+          size={"3"}
+          onChange={(event) => setPassword(event.target.value)}
+          required
+          autoComplete="new-password"
+        />
 
-          <InputError messages={errors.email} className="mt-2" />
-        </div>
-
-        {/* Password */}
-        <div className={styles.field}>
-          <Label htmlFor="password">
-            <Text weight="medium">Password</Text>
-          </Label>
-
-          <TextField.Input
-            id="password"
-            type="password"
-            placeholder={"Enter Password"}
-            value={password}
-            size={"3"}
-            onChange={(event) => setPassword(event.target.value)}
-            required
-            autoComplete="new-password"
-          />
-
-          <InputError messages={errors.password} className="mt-2" />
-        </div>
-
-        {/* Confirm Password */}
-        <div className={styles.field}>
-          <Label htmlFor="passwordConfirmation">
-            <Text weight="medium">Confirm Password</Text>
-          </Label>
-
-          <TextField.Input
-            id="passwordConfirmation"
-            type="password"
-            placeholder={"Confirm Password"}
-            value={passwordConfirmation}
-            size={"3"}
-            onChange={(event) => setPasswordConfirmation(event.target.value)}
-            required
-            autoComplete="new-password"
-          />
-
-          <InputError messages={errors.password_confirmation} />
-        </div>
+        <InputField
+          label="Confirm Password"
+          id="passwordConfirmation"
+          type="password"
+          placeholder={"Confirm Password"}
+          value={passwordConfirmation}
+          size={"3"}
+          onChange={(event) => setPasswordConfirmation(event.target.value)}
+          required
+          autoComplete="new-password"
+        />
 
         <Button variant="solid" size="4">
           Save New Password
