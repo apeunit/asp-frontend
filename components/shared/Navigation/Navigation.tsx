54389e23c0d91006caa0ae7362e70910bda0a30e
Marcel Schulz <mail@schulzmarcel.de>
2024-03-13 12:26:49 +0100

    bring back logout menu

--- components/shared/Navigation/Navigation.tsx
+++ components/shared/Navigation/Navigation.tsx
@@ -2,8 +2,14 @@ import { usePathname, useRouter } from "next/navigation";
 import IconButton from "../IconButton/IconButton";
 import { ArrowLeft, Dots } from "../Icons/Icons";
 import styles from "./Navigation.module.css";
+import { useState } from "react";
+import { useAuth } from "@/hooks/auth";
+import { Button } from "@radix-ui/themes";
 
 const Navigation = () => {
+  const { user, logout } = useAuth({ middleware: "auth" });
+  // TODO: move menu state to context
+  const [open, setOpen] = useState(false);
   const pathname = usePathname();
   const router = useRouter();
 
@@ -15,19 +21,31 @@ const Navigation = () => {
     : null;
 
   return (
-    <header className={styles.container}>
-      <div>
-        {back && (
-          <IconButton onClick={back}>
-            <ArrowLeft />
-          </IconButton>
-        )}
-      </div>
-      {/* TODO: implement drop down menu */}
-      <IconButton onClick={() => null}>
-        <Dots />
-      </IconButton>
-    </header>
+    <>
+      <header className={styles.container}>
+        <div>
+          {back && (
+            <IconButton onClick={back}>
+              <ArrowLeft />
+            </IconButton>
+          )}
+        </div>
+        {/* TODO: implement drop down menu */}
+        <IconButton onClick={() => setOpen(!open)}>
+          <Dots />
+        </IconButton>
+      </header>
+      {/* Responsive Navigation Menu */}
+      {open && (
+        <div>
+          <div>{user?.name}</div>
+          <div>{user?.email}</div>
+
+          {/* Authentication */}
+          <Button onClick={logout}>Logout</Button>
+        </div>
+      )}
+    </>
   );
 };
 
