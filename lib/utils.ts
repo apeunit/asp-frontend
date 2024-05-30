ffe9249020c87401cb2355f5fbbf674970d00624
Bernd Große Lordemann <bernd@newnow.de>
2024-05-30 13:29:00 +0200

    fix: increase Maps Link Zoom Factor

--- lib/utils.ts
+++ lib/utils.ts
@@ -26,7 +26,7 @@ export const getGoogleMapsLatLngLink = (
   latitude: string,
   longitude: string
 ) => {
-  return `https://www.google.com/maps/@${latitude},${longitude},14z`;
+  return `https://www.google.com/maps/@${latitude},${longitude},19z`;
 };
 
 export const TEMP_animationOptions = {
