4e4327511bfecbab68e41ec7331bdf7d4ed57bce
Jonath-z <jonathanzihindula95@gmail.com>
2024-06-06 20:41:46 +0200

    refactor: update app layout for header and user auth

--- views/app/layout.tsx
+++ views/app/layout.tsx
@@ -1,15 +1,15 @@
 import { useAuth } from "../../hooks/auth"
 import Loading from "./Loading"
 
-import styles from "./layout.module.css"
+import styles from "./Layout.module.css"
 import Header from "@/components/shared/Header/Header"
 
 const AppLayout = ({ children, header }: any) => {
-  // const { user } = useAuth({ middleware: "auth" })
+  const { user } = useAuth({ middleware: "auth" })
 
-  // if (!user) {
-  //   return <Loading />
-  // }
+  if (!user) {
+    return <Loading />
+  }
 
   return (
     <div className={styles.container}>
